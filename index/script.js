5cd808b5beaa3b37b7f2334b97d380a8e19b5fca
Barenza <leimer.c@outlook.de>
2025-03-19 12:16:12 +0100

    js changes

--- index/script.js
+++ index/script.js
@@ -3,10 +3,9 @@ const slides = document.querySelectorAll('.slide');
 
 function showNextSlide() {
   slides[currentIndex].classList.remove('active');
-  currentIndex = (currentIndex + 1) % slides.length; // Geht zur nächsten Folie
+  currentIndex = (currentIndex + 1) % slides.length; 
   slides[currentIndex].classList.add('active');
 }
 
-// Startet die Slideshow mit einem Intervall von 3 Sekunden
 slides[currentIndex].classList.add('active');
-setInterval(showNextSlide, 3000);
+setInterval(showNextSlide, 4500);
